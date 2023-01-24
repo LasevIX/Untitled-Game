cee154feb71666014fc226c72ad2e09f4a65d4e6
LasevIX_@Laptop-lOhkM <l73yx.off@gmail.com>
2023-01-24 21:53:22 +0100

    formatting

--- src/logic.py
+++ src/logic.py
@@ -1,5 +1,7 @@
 import logging
-from logging import INFO,WARNING,DEBUG,ERROR
+from logging import INFO, WARNING, DEBUG, ERROR
 
-logging.basicConfig(filename="game.log",format="%(asctime)s %(levelname)s :%(message)s")
-log=logging.log
+logging.basicConfig(
+    filename="game.log", format="%(asctime)s %(levelname)s :%(message)s"
+)
+log = logging.log
