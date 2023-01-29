da020b2cd09e9bb5c91b0a27624c3090f1479626
LasevIX_@Laptop-lOhkM <l73yx.off@gmail.com>
2023-01-29 13:37:39 +0100

    further advancements in rendering of floor

--- src/floor_rendering.py
+++ src/floor_rendering.py
@@ -2,8 +2,10 @@ from typing import Literal
 import pygame as pg
 import objects as o
 
+FLOOR_SIZE = FLOOR_WIDTH, FLOOR_HEIGHT = 720, 400
 
-def render_tilemap(
+
+def render(
     tilemap: o.tileMap,
     surface: pg.surface.Surface,
     view_pos_on_map: tuple[int, int],
@@ -12,3 +14,16 @@ def render_tilemap(
 ):
     if view_pos_on_surf == "center":
         view_pos_on_surf = (int(surface.get_width() / 2), int(surface.get_height() / 2))
+    tile_amount = (
+        round(surface.get_width() / px_per_tile),
+        round(surface.get_height() / px_per_tile),
+    )
+    mapslice: o.tileMap = tilemap.get_tile_view(
+        x=view_pos_on_map[0],
+        y=view_pos_on_map[1],
+        width=tile_amount[0],
+        height=tile_amount[1],
+    )
+    for i,col in enumerate(mapslice._maparray):
+        for j,tile in enumerate(col):
+            rect_to_blit=pg.rect.Rect(i*px_per_tile,j*px_per_tile,px_per_tile,px_per_tile)
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from pygame import locals as lc
 import objects as o
 import logic as l
 import ceiling_rendering as cr
+import floor_rendering as fr
 import sys
 l.log(l.INFO,"main: Starting execution, imports complete")
 SCREENSIZE=SCREENWIDTH,SCREENHEIGHT=720,480
@@ -11,13 +12,18 @@ done=False
 pg.init()
 DISPLAYSURF: pg.surface.Surface = pg.display.set_mode(SCREENSIZE)
 fps=pg.time.Clock()
+r=pg.rect.Rect((0,0,720,400))
+FLOORSURF=DISPLAYSURF.subsurface(r)
+tm=o.tileMap(size=(16,9))
+
 while not done:
-    DISPLAYSURF.fill("#1d1d1d")
+    DISPLAYSURF.fill("#000000")
     for event in pg.event.get():
         if event.type == lc.QUIT:
             l.log(l.INFO,"process ended with code",lc.QUIT)
             pg.quit()
             sys.exit()
-
+    fr.render(tm,FLOORSURF,(8,5),"center")
     pg.display.update()
     fps.tick(60)
+    
